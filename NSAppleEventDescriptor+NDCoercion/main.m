88360ed95890727c8d21633bfa3f1be0a3fa51aa
Nathan Day <nathan_day@mac.com>
2011-07-25 01:19:12 +1000

    Initial Commit

---
+++ NSAppleEventDescriptor+NDCoercion/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  NSAppleEventDescriptor+NDCoercion
+//
+//  Created by Nathan Day on 25/07/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+	// insert code here...
+	NSLog(@"Hello, World!");
+
+	[pool drain];
+    return 0;
+}
+
