939aa9d369c9c2fe43f27d263fa887775d935840
Nathan Day <nathan_day@mac.com>
2011-07-25 01:27:22 +1000

    Split out from NDScript, added very simple test

--- NSAppleEventDescriptor+NDCoercion/main.m
+++ NSAppleEventDescriptor+NDCoercion/main.m
@@ -1,22 +1,33 @@
-//
-//  main.m
-//  NSAppleEventDescriptor+NDCoercion
-//
-//  Created by Nathan Day on 25/07/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
+/*
+ main.m
+ NSAppleEventDescriptor+NDScriptData
+ 
+ Created by Nathan Day on 24/07/11.
+ Copyright 2011 Nathan Day. All rights reserved.
+ */
 
 #import <Foundation/Foundation.h>
+#import "NSAppleEventDescriptor+NDCoercion.h"
+
+static NSString			* const kTestAppleScript = @"return {alph:\"Albert\", beta:2, gama:{3, true, \"Gary\", alias \"Laptop HD:Applications:\"}}";
+
+static void testBasicCoercion();
 
 int main (int argc, const char * argv[])
 {
-
+	
 	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-
-	// insert code here...
-	NSLog(@"Hello, World!");
-
+	testBasicCoercion();
 	[pool drain];
-    return 0;
+	return 0;
 }
 
+void testBasicCoercion()
+{
+	NSAppleScript				* theScript = [[NSAppleScript alloc] initWithSource:kTestAppleScript];
+	NSDictionary				* theErrorDictionary = nil;
+	NSAppleEventDescriptor		* theResult = [theScript executeAndReturnError:&theErrorDictionary];
+	NSLog( @"Got result: %@", theResult.objectValue  );
+	
+	[theScript release];
+}
